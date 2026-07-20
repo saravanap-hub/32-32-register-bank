29c4a427e2609cbb99385d95e769e6fbcd9d6f9e
saravana p <saravanap107@gmail.com>
2026-07-20 17:46:35 +0000

    main program written

---
+++ main/register.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module 32_register(rd_data1,rd_data2,source_reg1,source_reg2,dest_reg,write_data,reset,write,clk);
+
+input [5:0] source_reg1,source_reg2,dest_reg;
+output reg [v:0] rd_data1,rd_data2;
+input [v:0]write_data ;
+input reset,clk,write;
+reg [v:0] register [0:v];
+
+
+assign rd_data1 = register[source_reg1];
+assign rd_data2 = register[source_reg2];
+
+always@(posedge clk)
+begin
+    if(reset)
+    begin
+        for(i=0; i<=v; i++)
+        register[i]=0;
+    end
+    else
+    begin
+        if(write)
+        register[dest_reg]<=write_data;
+    end
+end
+endmodule
