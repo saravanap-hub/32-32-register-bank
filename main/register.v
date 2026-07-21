f58cd00f5741fda158c5ae50109540b121bccfeb
saravana <saravanap107@gmail.com>
2026-07-22 00:09:15 +0530

    modified tb

--- main/register.v
+++ main/register.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module 32_register(rd_data1,rd_data2,source_reg1,source_reg2,dest_reg,write_data,reset,write,clk);
+module register_32(rd_data1,rd_data2,source_reg1,source_reg2,dest_reg,write_data,reset,write,clk);
 
 parameter v = 31 ;
 
@@ -8,7 +8,7 @@ input [4:0] source_reg1,source_reg2,dest_reg;
 input [v:0]write_data ;
 input reset,clk,write;
 
-output reg [v:0] rd_data1,rd_data2;
+output [v:0] rd_data1,rd_data2;
 
 reg [v:0] register [0:v];
 integer i;
--- main/tb_register.v
+++ main/tb_register.v
@@ -2,13 +2,13 @@
 
 module tb_reg;
 reg [4:0] source_reg1,source_reg2,dest_reg;
-reg [v:0]write_data ;
+reg [31:0]write_data ;
 reg reset,clk,write;
 integer i;
 
-wire [v:0] rd_data1,rd_data2;
+wire [31:0] rd_data1,rd_data2;
 
-32_register Re (rd_data1,rd_data2,source_reg1,source_reg2,dest_reg,write_data,reset,write,clk);
+register_32 Re(rd_data1,rd_data2,source_reg1,source_reg2,dest_reg,write_data,reset,write,clk);
 
 initial
 clk=0;
@@ -18,21 +18,30 @@ always #5 clk=~clk;
 initial
 begin
     reset=1; write=0; 
-    #8 reset=0; write=1; 
+    #3 reset=0; write=1; 
+end
+
+initial
+begin
     for(i=0;i<=31;i=i+1)
     begin
-        #2 dest_reg=i; write_data=i*10;
+        #4 dest_reg=i; write_data=i*10;
+        #10 write=0; 
     end
+
+    #20;
     for(i=2;i<=31;i=i+2)
     begin
-        #2 source_reg1=i-1; source_reg2=i;
+        source_reg1=i-1; source_reg2=i;
+        #5;
     end
 end
 
 initial
 begin
+    $monitor("time=%0d, source_reg1=%0d, source_reg2=%0d, dest_reg=%0d, write_data=%0d, rd_data1=%0d, rd_data2=%0d",$time,source_reg1,source_reg2,dest_reg,write_data,Re.rd_data1,Re.rd_data2);
     $dumpfile("tb_reg.vcd");
     $dumpvars(0,tb_reg);
-    #100 $finish;
+    #1000 $finish;
 end
-
+endmodule
