41cd2ce426f557d2f942827afa4b671a094deb67
saravana <saravanap107@gmail.com>
2026-07-23 23:16:57 +0530

    read data bug fixed

--- main/tb_register.v
+++ main/tb_register.v
@@ -18,29 +18,29 @@ always #5 clk=~clk;
 initial
 begin
     reset=1; write=0; 
-    #3 reset=0; write=1; 
+    #6 reset=0; write=1; 
 end
 
 initial
 begin
     for(i=0;i<=31;i=i+1)
     begin
-        #4 dest_reg=i; write_data=i*10;
-        #10 write=0; 
+        #8 dest_reg=i; write_data=i*10;
+         $display("time=%0d, source_reg1=%0d, source_reg2=%0d, dest_reg=%0d, write_data=%0d, reg=%0d, rd_data1=%0d, rd_data2=%0d",$time,source_reg1,source_reg2,dest_reg,write_data,Re.register[i],rd_data1,rd_data2);
     end
 
-    #20;
-    for(i=2;i<=31;i=i+2)
+    #20 write=0;
+    for(i=0;i<=30;i=i+1)
     begin
-        source_reg1=i-1; source_reg2=i;
-        #5;
+        source_reg1=i; source_reg2=i+1;
+         $display("time=%0d, source_reg1=%0d, source_reg2=%0d, dest_reg=%0d, write_data=%0d, reg=%0d, rd_data1=%0d, rd_data2=%0d",$time,source_reg1,source_reg2,dest_reg,write_data,Re.register[i],rd_data1,rd_data2);
+        #4;
     end
 end
 
 initial
 begin
-    $monitor("time=%0d, source_reg1=%0d, source_reg2=%0d, dest_reg=%0d, write_data=%0d, rd_data1=%0d, rd_data2=%0d",$time,source_reg1,source_reg2,dest_reg,write_data,rd_data1,rd_data2);
-    $dumpfile("tb_reg.vcd");
+   $dumpfile("tb_reg.vcd");
     $dumpvars(0,tb_reg);
     #1000 $finish;
 end
